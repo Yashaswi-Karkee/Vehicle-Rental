26f0d8c70eea7318dd611baa8122ca885d74d7e4
Yashaswi Karkee <yashaswi.karkee007@gmail.com>
2023-09-26 10:22:06 +0545

    usrprofile updated

--- resources/views/userProfile.blade.php
+++ resources/views/userProfile.blade.php
@@ -13,20 +13,20 @@
 <body class="bg-gray-100 font-sans">
     <div class="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
         <!-- User Profile and Ride Request Container -->
-        <div class="bg-white p-6 rounded shadow-lg w-full max-w-xl flex">
+        <div class="bg-white p-6 rounded shadow-lg w-full max-w-xl flex flex-col">
+            <!-- Close Icon (Top Right Corner) -->
+            <a class="absolute top-2 right-2 text-gray-500 hover:text-gray-800" href="{{ route('homepage') }}">
+                <i class="fas fa-times text-2xl"></i>
+            </a>
             <!-- User Profile Content -->
-            <div class="w-2/3 pr-4">
-                <!-- Close Icon (Top Right Corner) -->
-                <button class="absolute top-2 right-2 text-gray-500 hover:text-gray-800" onclick="closeProfile()">
-                    <i class="fas fa-times"></i>
-                </button>
+            <div id="profileContent" class="w-full block">
                 <!-- User Profile Header -->
-                <div class="flex items-center mb-6">
-                    <div class="mr-4">
+                <div class="w-full flex flex-col justify-center items-center mb-8">
+                    <div class="flex justify-center mt-4">
                         <img src="profile-pic.jpg" alt="User Profile Picture"
-                            class="w-16 h-16 rounded-full object-cover">
+                            class="w-16 h-16 rounded-full object-cover mb-8">
                     </div>
-                    <div>
+                    <div class="flex justify-center flex-col items-center">
                         <h2 class="text-xl font-semibold text-gray-800">User Name</h2>
                         <p class="text-gray-600">user@email.com</p>
                     </div>
@@ -40,13 +40,13 @@
                         City, Country</p>
                 </div>
                 <div class="mb-6">
-                    <h3 class="text-lg font-semibold text-gray-800">Map</h3>
+                    <h3 class="text-lg font-semibold text-gray-800 mb-4">Map</h3>
                     <!-- Map (You can replace this with your map component) -->
-                    <div id="map" class="w-full h-40 bg-gray-300"></div>
+                    <div id="map" class="w-full h-56 bg-gray-300"></div>
                 </div>
             </div>
             <!-- Ride Request Content -->
-            <div class="w-8/12 pl-4">
+            <div id="rideRequestContent" class="w-8/12 pl-4" style="display: none;">
                 <h3 class="text-lg font-semibold text-gray-800 mb-4">Ride Requests</h3>
                 <!-- Scrollable Ride Requests (Replace with actual requests) -->
                 <div class="h-64 overflow-y-scroll">
@@ -65,7 +65,35 @@
                     <!-- Add more ride request items as needed -->
                 </div>
             </div>
+            <!-- Toggle Button (Switch between Profile and Ride Requests) -->
+            <button
+                class="bg-indigo-500 text-white hover:bg-indigo-600 transition duration-300 rounded-full py-2 px-4 mt-2"
+                id="toggle">View Ride Requests</button>
         </div>
+
+        <!-- JavaScript to Toggle Profile and Ride Requests -->
+        <script>
+            // Function to toggle between Profile and Ride Requests
+            const toggle = document.getElementById('toggle');
+
+            function toggleView() {
+                const profileContent = document.querySelector('#profileContent');
+                const rideRequestContent = document.querySelector('#rideRequestContent');
+
+                // Toggle visibility of Profile and Ride Requests
+                if (toggle.innerHTML === 'View Ride Requests') {
+                    profileContent.style.display = 'none';
+                    rideRequestContent.style.display = 'block';
+                    toggle.innerHTML = "View Profile";
+                } else {
+                    profileContent.style.display = 'block';
+                    rideRequestContent.style.display = 'none';
+                    toggle.innerHTML = "View Ride Requests";
+                }
+            }
+
+            toggle.addEventListener("click", toggleView);
+        </script>
     </div>
 </body>
 
